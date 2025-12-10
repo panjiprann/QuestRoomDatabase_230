38426b696ba7dd32f45c604025c033998186ac32
panjiprann <panjipranantya20@gmail.com>
2025-12-10 21:47:01 +0700

    Add DestinasiDetailSiswa navigation object
    Introduced the `DestinasiDetailSiswa` object in `DestinasiEditSiswa.kt` to handle navigation for the student detail view. This includes definitions for the route, title resource, item ID argument, and the complete route with arguments.

--- app/src/main/java/com/example/myroom/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/myroom/view/route/DestinasiDetailSiswa.kt
@@ -1,2 +1,10 @@
 package com.example.myroom.view.route
 
+import com.example.myroom.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
